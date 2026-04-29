bc14667526b79b61735aec991cb059b83db0dd03
kzancle <kevinsucle@gmail.com>
2026-04-29 09:01:14 -0300

    ADD validation pass strength

--- public/js/functions.js
+++ public/js/functions.js
@@ -10,3 +10,39 @@ function existsVerify(input, item){
     let exists = input.includes(item)
     return exists
 }
+
+function existsSpecial(pass){
+    let specials = ["!","@","#","$","%","¨","&","*", "(", ")", "-", "_", "+", "-", "=", "?",">", "<"]
+    let exists = false
+    for (let i = 0; i < specials.length; i++) {
+        if(pass.includes(specials[i])){
+            exists = true
+        }
+    }
+    return exists
+}
+function pontuationPass(pass){
+        let points = 0
+        let reSpecial = new RegExp("/[a-zA-Z0-9]/g+$");
+        let reUpper = new RegExp("[A-Z]");
+        let reNumber = new RegExp("[0-9]");
+        if(pass.length>8){
+            points+= 1
+        }
+
+        if(reUpper.test(pass)){
+            points+=1
+        }
+
+        if(reNumber.test(pass)){
+            points+=1
+        }
+
+        if(existsSpecial(pass) == true){
+            points+=1
+        }
+
+        
+
+        return points
+    }
